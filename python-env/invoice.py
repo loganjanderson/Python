98da2488004d6eecd037a0d87dbb72c2de78c822
loganjanderson <loganjohnanderson@gmail.com>
2021-01-20 11:11:02 -0700

    python invoice

---
+++ python-env/invoice.py
@@ -0,0 +1,11 @@
+class Invoice:
+
+    def __init__(self, client, total):
+        self.client = client
+        self.total = total
+
+    def formatter(self):
+        return f'{self.client} owes: {self.total}'
+
+google = Invoice('Google', 100)
+print(google.formatter())
